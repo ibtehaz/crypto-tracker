ae681ad4f33514bbf1ac989e0ed6c5fafb5dd4c0
ibtehaz <ibtehaz.utsay@gmail.com>
2022-04-12 23:59:43 -0700

    initiate dependencies and components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Typography, Menu, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar

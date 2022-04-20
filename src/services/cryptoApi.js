4997895902aff42b06bca0cd409ea9c7f5807d2f
ibtehaz <ibtehaz.utsay@gmail.com>
2022-04-19 23:15:16 -0700

    crypto details page completed without linechart

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,10 +15,16 @@ export const cryptoApi = createApi({
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
         })
     })
 });
 
 export const {
-    useGetCryptosQuery,
+    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
 } = cryptoApi;

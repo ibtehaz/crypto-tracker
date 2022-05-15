877afe1453790bffb59893b39dc43188249321a1
ibtehaz <ibtehaz.utsay@gmail.com>
2022-05-15 00:58:29 -0700

    improve responsiveness and added navbar dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Button, Typography, Menu, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
@@ -11,6 +11,27 @@ import {
 import icon from "../images/crypto.png";
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -18,7 +39,9 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Crypto Tracker</Link> 
         </Typography.Title>
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
       </div>
+      {activeMenu && (
       <Menu theme="dark">
         <Menu.Item icon={<HomeOutlined />}>
           <Link to="/">Home</Link>
@@ -34,6 +57,7 @@ const Navbar = () => {
         </Menu.Item>
         
       </Menu>
+      )}
     </div>
   );
 };

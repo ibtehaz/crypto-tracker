64935a01ef368fa492d1d289e09d94280ad2e4ec
Mihai Mihailescu <mihai.mike92@gmail.com>
2022-05-15 18:44:38 -0700

    Search by uuid. Curated list.

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,14 +2,63 @@ import React, { useEffect, useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
-import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useGetCryptosByUUIDQuery, useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = ({ simplified }) => {
-  const count = simplified ? 10 : 100;
-  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  //Simplified version gives the curated list, otherwise display 100 coins
+  let queryString;
+  const count = simplified ? queryString="" : 100;
+
+  if (simplified) {
+
+    //Searching by UUID of each coin rather than by symbol.
+    const preferred = [
+      {
+        name: "BTC",
+        uuid: "Qwsogvtv82FCd"
+      },
+      {
+        name: "ETH",
+        uuid: "razxDUgYGNAdQ"
+      },
+      {
+        name: "XRP",
+        uuid: "-l8Mn2pVlRs-p"
+      },
+      {
+        name: "SOL",
+        uuid: "zNZHO_Sjf"
+      },
+      {
+        name: "ADA",
+        uuid: "qzawljRxB5bYu"
+      },
+      {
+        name: "DOGE",
+        uuid: "a91GCGd_u96cF"
+      },
+      {
+        name: "SHIB",
+        uuid: "xz24e0BjL"
+
+      }
+    ]
+    //Build the query string before passing it to the request builder.
+    preferred.forEach(
+      (coin) => {
+        queryString += '&uuids[]=' + coin.uuid
+      });
+    var { data: cryptosList, isFetching } = useGetCryptosByUUIDQuery(queryString);
+  }
+  else {
+    var { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  }
+
+  // Searching by symbols can result in coins with null values making parsing difficult.
+  // const { data: cryptosPrefList, isFetching } = useGetCryptoBySymbolQuery("BTC,DOGE,SHIB");
+
   const [cryptos, setCryptos] = useState();
   const [searchTerm, setSearchTerm] = useState('');
-
 
   useEffect(() => {
     setCryptos(cryptosList?.data?.coins);
@@ -19,7 +68,8 @@ const Cryptocurrencies = ({ simplified }) => {
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
 
-  if(isFetching) return <div>Loading...</div>;
+  if (isFetching) return <div>Loading...</div>;
+
 
 
   return (
